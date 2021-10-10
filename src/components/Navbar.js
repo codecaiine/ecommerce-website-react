0e76446d15b176a801643ce9cf4f9b1f4f3ec8b7
codecaiine <akabrouyannickn@gmail.com>
2021-10-10 21:41:04 +0000

    Style navbar component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+    height: 60px;
+`;
+
+const Wrapper = styled.div`
+    padding: 20px 10px;
+`;
 
 const Navbar = () => (
-  <div>
-    Navbar
-  </div>
+  <Container>
+    <Wrapper>
+      Navbar
+    </Wrapper>
+  </Container>
 );
 
 export default Navbar;
