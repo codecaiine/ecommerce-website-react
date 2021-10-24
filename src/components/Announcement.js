82bac53ffd5480729d7d574b1e42eeca76d03c09
codecaiine <akabrouyannickn@gmail.com>
2021-10-24 22:25:07 +0000

    Add style to announcement

--- src/components/Announcement.js
+++ src/components/Announcement.js
@@ -3,6 +3,11 @@ import styled from 'styled-components';
 const Container = styled.div`
     height: 30px;
     background-color: teal;
+    color: white;
+    display: flex;
+    align-items: center;
+    font-size: 14px;
+    font-weight: 500; 
 `;
 
 const Announcement = () => (
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import Announcement from '../components/Announcement';
 import Navbar from '../components/Navbar';
 
 const Home = () => (
   <div>
+    <Announcement />
     <Navbar />
   </div>
 );
