7d258867c455fbbd2714dc945dcb0f1a0f02139d
Yannick-Noel AKA <akabrouyannickn@gmail.com>
2022-01-09 20:50:06 +0000

    Add basic style to slider container

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -1,10 +1,16 @@
-import React from 'react'
+import styled from "styled-components"
+
+const Container = styled.div `
+width: 100%;
+height: 100vh;
+display: flex;
+`
 
 const Slider = () => {
     return (
-        <div>
+        <Container>
             
-        </div>
+        </Container>
     )
 }
 
