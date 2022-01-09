c05e4768c2a223f70e94c12b6606a14efcf106b0
Yannick-Noel AKA <akabrouyannickn@gmail.com>
2022-01-09 20:59:08 +0000

    Add arrow left and right icons

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -1,17 +1,22 @@
-import styled from "styled-components"
+import { ArrowLeftOutlined, ArrowRightOutlined } from '@material-ui/icons';
+import styled from 'styled-components';
 
-const Container = styled.div `
+const Container = styled.div`
 width: 100%;
 height: 100vh;
 display: flex;
-`
+`;
 
-const Slider = () => {
-    return (
-        <Container>
-            
-        </Container>
-    )
-}
 
-export default Slider
+const Slider = () => (
+  <Container>
+    <Arrow>
+      <ArrowLeftOutlined />
+    </Arrow>
+    <Arrow>
+      <ArrowRightOutlined/>
+    </Arrow>
+  </Container>
+);
+
+export default Slider;
