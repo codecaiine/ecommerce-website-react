8246a1251c002d1a078f1db049cae9d40be03c4f
Yannick-Noel AKA <akabrouyannickn@gmail.com>
2022-01-10 23:56:58 +0000

    Declare handleClick event

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -74,6 +74,13 @@ cursor: pointer;
 
 const Slider = () => {
   const [slideIndex, setSlideIndex] = useState(0);
+  const handleClick = (direction) => {
+    if (direction === "left") {
+      setSlideIndex(slideIndex > 0 ? slideIndex - 1 : 2);
+    } else {
+      setSlideIndex(slideIndex < 2 ? slideIndex + 1 : 0);
+    }
+  };
 
   return (
     <Container>
