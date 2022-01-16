7aa7520909ca6e05b309891b9815f9b157986a6c
Yannick-Noel AKA <akabrouyannickn@gmail.com>
2022-01-16 23:38:09 +0000

    Fix linters errors

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -2,7 +2,11 @@ import { styled } from '@material-ui/core';
 import { categories } from '../data';
 import CategoryItem from './CategoryItem';
 
-const Container = styled.div``;
+const Container = styled.div`
+  display: flex;
+  padding: 20px;
+  justify-content: space-between;
+`;
 
 const Categories = () => (
   <Container>
