5d88208858668eeed021043ce7c897ab2b8c2b1e
Yannick-Noel AKA <akabrouyannickn@gmail.com>
2022-01-16 22:01:31 +0000

    Import category Item into Categorie

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,16 +1,15 @@
 import { styled } from '@material-ui/core';
 import { categories } from '../data';
+import CategoryItem from './CategoryItem';
 
 const Container = styled.div``;
 
-const Categories = () => {
-    return (
-      <Container>
-        {categories.map((item) => (
-          <CategoryItem item={item} key={item.id} />
-        ))}
-      </Container>
-    );
-  };
-  
-  export default Categories;
+const Categories = () => (
+  <Container>
+    {categories.map((item) => (
+      <CategoryItem item={item} key={item.id} />
+    ))}
+  </Container>
+);
+
+export default Categories;
