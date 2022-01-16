be3771ac596af7e0d12c567b2493ebb8e5c0abcd
Yannick-Noel AKA <akabrouyannickn@gmail.com>
2022-01-16 22:09:59 +0000

    Add image details and button

--- src/components/CategoryItem.js
+++ src/components/CategoryItem.js
@@ -2,8 +2,14 @@ import { styled } from '@material-ui/core';
 
 const Container = styled.div``;
 
-const CategoryItem = () => (
-  <Container />
+const CategoryItem = ({ item }) => (
+  <Container>
+    <Image src={item.img} />
+    <Details>
+      <Title>{item.title}</Title>
+      <Button>SHOP NOW</Button>
+    </Details>
+  </Container>
 );
 
 export default CategoryItem;
